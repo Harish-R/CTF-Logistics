8573ff717df8e22483eef7a8f46dd67aefd476ea
Harish-R <ferrarixx599@gmail.com>
2015-09-17 02:18:54 +0530

    Initial Commit

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,27 @@
+class ItemsController < ApplicationController
+  
+  def create
+  end
+
+  def add
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+  def plus
+  end
+
+  def minus
+  end
+
+  def admin_dashboard
+  end
+
+  def user_dashboard
+  end
+
+end
